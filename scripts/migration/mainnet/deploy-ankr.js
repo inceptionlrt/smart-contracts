4eae288f8bb2bed1dd7e3eee9be796e0fe964e6f
mellaught <torres-dan@yandex.ru>
2024-01-17 16:48:18 +0100

    added ankrEth asset and corresponding vault, tests

---
+++ scripts/migration/mainnet/deploy-ankr.js
@@ -0,0 +1,17 @@
+const { addresses } = require("./config-addresses");
+const { deployVault } = require("../deploy-vault");
+
+const VaultName = "InankrEthVault",
+  TokenName = "Inception Restaked ankrETH",
+  TokenSymbol = "InankrETH";
+
+async function main() {
+  await deployVault(addresses, VaultName, TokenName, TokenSymbol);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
