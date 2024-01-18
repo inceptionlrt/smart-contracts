954cde6e663d2d1f71b714546c3a9ab2c1786023
mellaught <torres-dan@yandex.ru>
2024-01-18 10:06:52 +0100

    Inception vault for Origin Protocol [WIP]

---
+++ contracts/tests/OriginProtocol/VaultCore.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract VaultCore {
+    function mint(
+        address _asset,
+        uint256 _amount,
+        uint256 _minimumOusdAmount
+    ) external {}
+}
