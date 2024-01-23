8f36ab9dc951f32282764c2773196e401a29c148
mellaught <torres-dan@yandex.ru>
2024-01-23 18:56:47 +0100

    added the migration file for cbETH

---
+++ scripts/migration/mainnet/deploy-cb.js
@@ -0,0 +1,21 @@
+/**
+ * Coinbase Ethereum LST
+ */
+
+const { addresses } = require("./config-addresses");
+const { deployVault } = require("../deploy-vault");
+
+const VaultName = "IncbEthVault",
+  TokenName = "Inception Restaked cbETH",
+  TokenSymbol = "IncbETH";
+
+async function main() {
+  await deployVault(addresses, VaultName, TokenName, TokenSymbol);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
