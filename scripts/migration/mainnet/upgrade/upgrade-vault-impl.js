d00a6be4a05dec9e91c734cb0889b302a11177c1
mellaught <torres-dan@yandex.ru>
2024-02-01 17:50:31 +0100

    updated the migration script with the missed vaults

--- scripts/migration/mainnet/upgrade/upgrade-vault-impl.js
+++ scripts/migration/mainnet/upgrade/upgrade-vault-impl.js
@@ -2,11 +2,22 @@ const { ethers, upgrades } = require("hardhat");
 const helpers = require("@nomicfoundation/hardhat-network-helpers");
 const { readJsonFiles, getVaultImplAndStrategyAddress } = require("../../../utils");
 
+const notScheduled = [
+  "0xC0660932C5dCaD4A1409b7975d147203B1e9A2B6",
+  "0xA9F8c770661BeE8DF2D026edB1Cb6FF763C780FF",
+  "0x4878F636A9Aa314B776Ac51A25021C44CAF86bEd",
+  "0xfE715358368416E01d3A961D3a037b7359735d5e",
+  "0x36B429439AB227fAB170A4dFb3321741c8815e55",
+];
+
 async function main() {
   // get all current vaults
   vaults = await readJsonFiles("./scripts/migration/addresses");
   for (const [vaultName, vaultData] of vaults) {
     const vaultAddress = vaultData.iVaultAddress;
+    if (!notScheduled.includes(vaultAddress)) {
+      continue;
+    }
     console.log(`Upgdading the InceptionVault:: ${vaultName} with the address: ${vaultAddress}`);
 
     const [implName] = await getVaultImplAndStrategyAddress(vaultName);
@@ -27,8 +38,8 @@ const upgradeVaultImpl = async (vaultAddress, newImpl) => {
   const transaction = await proxyAdmin.upgradeAndCall.populateTransaction(vaultAddress, newImpl, "0x");
 
   const res = await timelock.schedule(transaction.to, transaction.value || "0", transaction.data, ethers.ZeroHash, ethers.ZeroHash, delay);
-  await res.wait();
-  console.log(`SHEDULE TX for ${vaultAddress}: ${res}`);
+  const receipt = await res.wait();
+  console.log(`SHEDULED TX for ${vaultAddress}: ${receipt}`);
 };
 
 const getNewImplFor = async (vaultAddress, newImpl) => {
