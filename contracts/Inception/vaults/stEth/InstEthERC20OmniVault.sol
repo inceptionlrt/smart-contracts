23176b852033fe5f9bef3046c448480452b5ad67
neanvo <neanvo@protonmail.com>
2024-06-28 04:54:58 +0300

    feat: added similar tests

---
+++ contracts/Inception/vaults/stEth/InstEthERC20OmniVault.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import "../InceptionERC20OmniVault.sol";
+import "../../../interfaces/IStEth.sol";
+
+/// @author The InceptionLRT team
+/// @title The InstEthOmniVault, specifically designed for the Lido Ethereum LST
+contract InstEthERC20OmniVault is InceptionERC20OmniVault {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(
+        IInceptionToken _inceptionToken,
+        IERC20 baseAsset
+    ) external initializer {
+        __InceptionERC20OmniVault_init(
+            "InstEthERC20OmniVault",
+            _inceptionToken,
+            baseAsset
+        );
+    }
+}
