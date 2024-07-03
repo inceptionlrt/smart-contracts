5e13f57f87c49b926fa41d6f6985da114778b7c1
mellaught <torres-dan@yandex.ru>
2024-07-03 17:57:19 +0200

    added the library deployment

--- scripts/migration/mainnet/upgrade/upgrade-vaults-v2.js
+++ scripts/migration/mainnet/upgrade/upgrade-vaults-v2.js
@@ -8,18 +8,32 @@ const InVault_E2 = "0x814CC6B8fd2555845541FB843f37418b05977d8d";
 const InVault_E1 = "0x36B429439AB227fAB170A4dFb3321741c8815e55";
 
 async function main() {
-  await upgradeInceptionVault("flash_withdrawal", InVault_E2, "InVault_E2");
-
-  await upgradeInceptionVault("flash_withdrawal", InVault_E1, "InVault_E1");
-}
-
-const upgradeInceptionVault = async (upgradeName, address, vaultImplContract) => {
   const [deployer] = await ethers.getSigners();
   console.log("Address of the Contract to be upgraded:", address);
   console.log("Upgrading with the account:", deployer.address);
 
-  const iVaultFactory = await hre.ethers.getContractFactory(vaultImplContract);
-  const impl = await upgrades.prepareUpgrade(address, iVaultFactory, { kind: "transparent" });
+  const libFactory = await ethers.getContractFactory("InceptionLibrary");
+  const lib = await libFactory.deploy();
+  await lib.waitForDeployment();
+  const libAddress = await lib.getAddress();
+  console.log("InceptionLibrary deployed to:", libAddress);
+
+  await upgradeInceptionVault(libAddress, "flash_withdrawal", InVault_E2, "InVault_E2");
+
+  await upgradeInceptionVault(libAddress, "flash_withdrawal", InVault_E1, "InVault_E1");
+}
+
+const upgradeInceptionVault = async (libAddress, upgradeName, address, vaultImplContract) => {
+  const iVaultFactory = await ethers.getContractFactory(vaultImplContract, {
+    libraries: {
+      InceptionLibrary: libAddress,
+    },
+  });
+  const impl = await upgrades.prepareUpgrade(address, iVaultFactory, {
+    kind: "transparent",
+    unsafeAllowLinkedLibraries: true,
+    unsafeSkipStorageCheck: true,
+  });
   console.log(`New Impl of InceptionVault(${impl}) was deployed`);
 
   const proxyAdmin = await upgrades.erc1967.getAdminAddress(address);
