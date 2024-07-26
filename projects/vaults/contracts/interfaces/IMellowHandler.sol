7fd1563a3a2b30afe28ced0bd67f7d2fa75219f9
neanvo <neanvo@protonmail.com>
2024-07-26 11:02:28 +0300

    feat: mellow integration

---
+++ projects/vaults/contracts/interfaces/IMellowHandler.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import "./IMellowVault.sol";
+
+interface IMellowHandler {
+    function depositMellow(
+        uint256 amount,
+        uint256 minLpAmount,
+        uint256 deadline
+    ) external returns (uint256 lpAmount);
+
+    function withdrawMellow(uint256 lpAmount, uint256 minLpAmount, uint256 deadline, uint256 requestDeadline, bool closePrevious) external;
+
+    function claimMellowWithdrawal(uint256 lpAmount) external;
+
+    function pendingMellowRequest() external returns (IMellowVault.WithdrawalRequest memory);
+}
