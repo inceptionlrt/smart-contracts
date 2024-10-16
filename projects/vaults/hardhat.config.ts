151fd549251631a0c136c9f5b9bd3a01b0ded0ad
Paul Fomichov <pavel.f@tagus.xyz>
2024-10-16 16:49:20 +0100

    updates

--- projects/vaults/hardhat.config.ts
+++ projects/vaults/hardhat.config.ts
@@ -20,7 +20,7 @@ const config: HardhatUserConfig = {
     hardhat: {
       forking: {
         url: process.env.RPC_URL_ARBITRUM_TESTNET || "",
-        blockNumber: 88842859,
+        blockNumber: 89177226,
       },
       accounts: [{ privateKey: `${process.env.DEPLOYER_PRIVATE_KEY}`, balance: "10000365467355464286459" }],
       chainId: 1337,  // Local chain ID for Hardhat network
