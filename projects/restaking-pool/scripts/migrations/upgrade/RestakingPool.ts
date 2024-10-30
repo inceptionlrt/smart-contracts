43e7e5e534d757284770b008125087a0ca6ff5fd
Paul I <scary.lynx@gmail.com>
2024-10-30 11:57:05 +0100

    added verification for RP new impl

--- projects/restaking-pool/scripts/migrations/upgrade/RestakingPool.ts
+++ projects/restaking-pool/scripts/migrations/upgrade/RestakingPool.ts
@@ -1,13 +1,13 @@
-import { ethers, upgrades } from "hardhat";
+import { ethers, upgrades, run } from "hardhat";
 
 async function main() {
     const [deployer] = await ethers.getSigners();
 
     const proxyAddress = "RESTAKING_POOL_PROXY_ADDRESS"; //TODO: change before firing the script
-    console.log(`Upgrading contract RestakingPool with account: ${deployer.address}`,);
+    console.log(`Upgrading contract RestakingPool with account: ${deployer.address}`);
     const network = await ethers.provider.getNetwork();
     console.log(`Network chain ID: ${network.chainId}`);
-    console.log(`RestakingPool proxy: ${proxyAddress}`,);
+    console.log(`RestakingPool proxy: ${proxyAddress}`);
 
     const RestakingPool = await ethers.getContractFactory("RestakingPool");
 
@@ -15,6 +15,20 @@ async function main() {
     const upgradedContract = await upgrades.upgradeProxy(proxyAddress, RestakingPool);
     await upgradedContract.waitForDeployment();
     console.log("Upgrade completed, new implementation deployed at:", upgradedContract.address);
+
+    try {
+        const implementationAddress = await upgrades.erc1967.getImplementationAddress(proxyAddress);
+        console.log("Implementation address to verify:", implementationAddress);
+
+        await run("verify:verify", {
+            address: implementationAddress,
+            constructorArguments: [],
+        });
+
+        console.log("Verification successful!");
+    } catch (error) {
+        console.error("Error during verification:", error);
+    }
 }
 
 main().catch((error) => {
