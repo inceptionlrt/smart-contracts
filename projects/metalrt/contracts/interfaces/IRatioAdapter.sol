67dd9c4f0ef84ebda02dacf21636beebbe56588c
davosloper <davosloper@gmail.com>
2024-11-01 18:14:37 +0500

    add/ratioadapter

---
+++ projects/metalrt/contracts/interfaces/IRatioAdapter.sol
@@ -0,0 +1,10 @@
+interface IRatioAdapter {
+
+    // --- Events ---
+    event TokenSet(address token, uint8 approach);
+    event RatioProviderSet(address token, address provider);
+
+    // --- Functions ---
+    function fromValue(address token, uint256 amount) external view returns (uint256);
+    function toValue(address token, uint256 amount) external view returns (uint256);
+}
