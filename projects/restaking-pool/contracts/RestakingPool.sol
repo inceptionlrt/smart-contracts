acc159a43cc2df64cf32ba9a07ca78c21fb7ae72
davosloper <davosloper@gmail.com>
2024-11-08 19:11:41 +0500

    div/claimer-as-param

--- projects/restaking-pool/contracts/RestakingPool.sol
+++ projects/restaking-pool/contracts/RestakingPool.sol
@@ -782,9 +782,9 @@ contract RestakingPool is
      * @param provider The new duration of the rewards timeline, measured in seconds.
      * @param newCoordinator The new reward coordinator address
      */
-    function setRewardCoordinator(string calldata provider, address newCoordinator) external onlyOperator {
+    function setRewardCoordinator(string calldata provider, address newCoordinator, address claimer) external onlyOperator {
         IRestaker restaker = IRestaker(_getRestakerOrRevert(provider));
-        restaker.__setRewardCoordinator(newCoordinator, config().getGovernance());
+        restaker.__setRewardCoordinator(newCoordinator, claimer);
     }
 
     /**
