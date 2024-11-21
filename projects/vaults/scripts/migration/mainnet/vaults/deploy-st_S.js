dd6fa3b02c3e4295db72f8d73e1b0341f3534ee3
davosloper <davosloper@gmail.com>
2024-11-21 14:43:32 +0500

    div/fixed_mainnet_script_S

--- projects/vaults/scripts/migration/mainnet/vaults/deploy-st_S.js
+++ projects/vaults/scripts/migration/mainnet/vaults/deploy-st_S.js
@@ -7,11 +7,10 @@ const VaultName = "inwstETHVault_S",
   // [P2P, MEV]
   mellowWrappers = ["0x41A1FBEa7Ace3C3a6B66a73e96E5ED07CDB2A34d", "0xdC1741f9bD33DD791942CC9435A90B0983DE8665"],
   mellowVaults = ["0x7a4EffD87C2f3C55CA251080b1343b605f327E3a", "0x5fD13359Ba15A84B76f7F87568309040176167cd"],
-  asset = "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84",
-  wrappedAddress = "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0";
+  asset = "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",
 
 async function main() {
-  await deployVault(addresses, VaultName, TokenName, TokenSymbol, mellowWrappers, mellowVaults, asset, wrappedAddress);
+  await deployVault(addresses, VaultName, TokenName, TokenSymbol, mellowWrappers, mellowVaults, asset);
 }
 
 main()
