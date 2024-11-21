356630d71ab4c03dd72c7705672d52ee083bb586
davosloper <davosloper@gmail.com>
2024-11-21 20:17:05 +0500

    div/added_vault_addresses_to_script

--- projects/vaults/scripts/migration/mainnet/vaults/deploy-st_S.js
+++ projects/vaults/scripts/migration/mainnet/vaults/deploy-st_S.js
@@ -4,8 +4,8 @@ const { deployVault } = require("../../deploy-vault_S");
 const VaultName = "inwstETHVault_S",
   TokenName = "Inception Symbiotic Restaked wstETH",
   TokenSymbol = "inwstETHs",
-  mellowWrappers = [],
-  mellowVaults = [],
+  mellowWrappers = ["0xdC1741f9bD33DD791942CC9435A90B0983DE8665"],
+  mellowVaults = ["0x5fD13359Ba15A84B76f7F87568309040176167cd"],
   asset = "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",
   ratioFeed = "0xFd73Be536503B5Aa80Bf99D1Fd65b1306c69B191";
 
