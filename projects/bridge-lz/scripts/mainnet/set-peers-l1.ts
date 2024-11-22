30715a53e2ccd08506e668f71b30f1e9ba1ec6d4
Paul Fomichov <pavel.f@tagus.xyz>
2024-11-22 14:22:49 +0000

    deployment scripts for l2

---
+++ projects/bridge-lz/scripts/mainnet/set-peers-l1.ts
@@ -0,0 +1,33 @@
+const { ethers } = require("hardhat");
+const fs = require("fs");
+const path = require("path");
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+
+    // Fetch addresses from the checkpoint files
+    const CrossChainBridgeEthereumAddress = ""; //TODO! Insert LZCrossChainBridgeL1 address here
+    const CrossChainBridgeOptimismAddress = ""; //TODO! Insert LZCrossChainBridgeL2 Optimism address here
+    const CrossChainBridgeArbitrumAddress = ""; //TODO! Insert LZCrossChainBridgeL2 Optimism address here
+
+    if (!CrossChainBridgeEthereumAddress || !CrossChainBridgeOptimismAddress || !CrossChainBridgeArbitrumAddress) {
+        throw new Error("CrossChainBridge addresses not found in the checkpoint files.");
+    }
+
+    // Get contract instances
+    const CrossChainBridgeSepolia = await ethers.getContractAt("LZCrossChainAdapterL1", CrossChainBridgeEthereumAddress);
+    const eIDArbitrumSepolia = 30110; //Arbitrum Mainnet eID
+    const eIDOptimismSepolia = 30111; //Sepolia Mainnet eID
+
+    // Set peer for OptimismSepolia
+    await CrossChainBridgeSepolia.connect(deployer).setPeer(eIDOptimismSepolia, ethers.utils.zeroPad(CrossChainBridgeOptimismAddress, 32));
+    await CrossChainBridgeSepolia.connect(deployer).setPeer(eIDArbitrumSepolia, ethers.utils.zeroPad(CrossChainBridgeArbitrumAddress, 32));
+    console.log("Peers set successfully");
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
