345a9dce26809ede292a54350021035aee76e8a8
Paul I <scary.lynx@gmail.com>
2024-12-09 15:11:21 +0100

    calculate ratio before

---
+++ projects/vaults/scripts/calculate_ratio_before.ts
@@ -0,0 +1,31 @@
+async function main2() {
+    /* 
+    As provided by Danil:
+    total supply = shares supply + l2_supply (last deposit)
+    total locked = pending balance + total staked + iov balance + nativeRebalancer balance
+    ratio = total supply / total locked
+    */
+
+    const sharesSupply = 989995800614486342517n; // value extracted from genesis-staking-service logs
+    const l2Supply = 0n; // currently L2 is synced with L1, means there is no "last deposit" unaccounted
+    const pendingBalance = 985214216742071116729n; // value extracted from RestakingPool.getPending() - 0xEAA6d9f33c7095218Ed9cD4f0D7FB6551A14005f
+    const totalStaked = 32000000000000000000n; // value extracted from genesis-staking-service logs
+    const iovBalance = 0n; // value extracted from InceptionOmniVault at address 0x55ec970B8629E01d26BAA7b5d092DD26784136bb
+    const nativeRebalancerBalance = 0n; // value extracted from NativeRebalancer at address 0xd13469584C26c329D56176b243f0507f84Fb778A
+
+    const totalSupply = sharesSupply + l2Supply;
+    console.log(`totalSupply: ${totalSupply}`);
+
+    const totalLocked = pendingBalance + totalStaked + iovBalance + nativeRebalancerBalance;
+    console.log(`totalLocked: ${totalLocked}`);
+
+    const precision = 10n ** 18n;
+    const ratio = (totalSupply * precision) / totalLocked;
+
+    console.log(`ratio (uint256 format): ${ratio.toString()}`);
+}
+
+main2().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
