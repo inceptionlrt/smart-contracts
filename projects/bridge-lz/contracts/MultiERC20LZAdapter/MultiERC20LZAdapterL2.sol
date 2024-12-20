f4d459f05280066ced28100308747120692cb10d
Svyatoslav Osin <s.osin@masterchain.ru>
2024-12-20 12:28:08 +0300

    wip multi-asset bridge

--- projects/bridge-lz/contracts/MultiERC20LZAdapter/MultiERC20LZAdapterL2.sol
+++ projects/bridge-lz/contracts/MultiERC20LZAdapter/MultiERC20LZAdapterL2.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.27;
 
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IGenericERC20Bridge} from "../interfaces/IGenericERC20Bridge.sol";
 
 contract MultiERC20LZAdapterL2 {
     struct ReportEntry {
@@ -12,8 +14,15 @@ contract MultiERC20LZAdapterL2 {
     }
 
     event ReportSubmitted(address indexed iovAddr, address indexed underlyingAssetAddr, uint256 incTokenSupply, uint256 uAssetBalance);
-    event SentToL1(); // todo
+    event ReportsSentToL1(); // todo
+    event BridgingComplete(); // todo
     event VaultAuthorizationChanged(address indexed vault, bool newStatus);
+    event BridgeAdded(address indexed asset, address indexed bridge);
+    event BridgingRequestSubmitted(address indexed asset, uint256 amount);
+    event BridgingFailed(address indexed asset, uint256 amount);
+    event EmergencyRecoveryAuthorized(address indexed authedAddr, address indexed asset, uint256 amount);
+
+    using SafeERC20 for IERC20;
 
     mapping(address=>bool) public authorizedVaults;
 
@@ -22,6 +31,12 @@ contract MultiERC20LZAdapterL2 {
     mapping(address=>uint256) public iovAddrToReportIdx;
     mapping(address=>uint256) public assetAddrToReportIdx;
     // if the value returned by these mappings is bigger than pendingRepCount, then it means this iov/asset hasn't submitted anything new
+
+    address[] public pendingAssetsToBridge;
+    uint256 public pendingBridgeCount;
+    mapping(address=>uint256) public pendingAssetAmounts;
+
+    mapping(address=>IGenericERC20Bridge) public bridges;
 
     modifier onlyAuthVaults() {
         require(authorizedVaults[msg.sender], "Not an authorized vault");
@@ -34,7 +49,15 @@ contract MultiERC20LZAdapterL2 {
         emit VaultAuthorizationChanged(_iov, _access);
     }
 
+    function setBridgeForAsset(address _asset, address _bridge) external /* onlyOwner */ {
+        require(_asset != address(0) && _bridge != address(0), "Zero addr");
+        bridges[_asset] = IGenericERC20Bridge(_bridge);
+        emit BridgeAdded(_asset, _bridge);
+    }
+
     function reportHoldings(address _asset, uint256 _incSupply, uint256 _assetBalance) external onlyAuthVaults {
+        require(address(bridges[_asset]) != address(0), "Unknown asset type");
+
         ReportEntry storage entry = pendingReports.push();
         iovAddrToReportIdx[msg.sender] = pendingRepCount;
         assetAddrToReportIdx[_asset] = pendingRepCount;
@@ -55,6 +78,34 @@ contract MultiERC20LZAdapterL2 {
         // do_lz_magic(pendingReports);
         pendingRepCount = 0;
 
-        emit SentToL1();
+        emit ReportsSentToL1();
+    }
+
+    function prepareBridging(address _asset, uint256 _amount) external onlyAuthVaults {
+        // Pull assets from iov for later batch bridging
+        IERC20(_asset).safeTransferFrom(msg.sender, address(this), _amount);
+        pendingAssetsToBridge.push() = _asset;
+        pendingAssetAmounts[_asset] = _amount;
+        pendingBridgeCount++;
+    }
+
+    function executeBridging() external /* onlyOwner */ {
+        for(uint256 i=0; i != pendingBridgeCount; ) {
+            address asset = pendingAssetsToBridge[i];
+            bool success = bridges[asset].bridge(pendingAssetAmounts[asset]);
+            if(!success) {
+                emit BridgingFailed(asset, pendingAssetAmounts[asset]);
+            } // soft fail in order to not stop the rest of the batch
+            pendingAssetAmounts[pendingAssetsToBridge[i]] = 0;
+            unchecked { ++i; }
+        }
+        pendingBridgeCount = 0;
+        emit BridgingComplete();
+    }
+
+    // Emergency recovery function. Authorizes owner to spend contract's tokens
+    function authorizeAssetRecovery(address _asset, uint256 _amount) external /* onlyOwner */ {
+        IERC20(_asset).approve(msg.sender, _amount);
+        emit EmergencyRecoveryAuthorized(msg.sender, _asset, _amount);
     }
 }
