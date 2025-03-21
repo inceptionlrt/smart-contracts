e35b6674eb3a513d7d56d331ccdd72ba693d451c
olexandr13 <oleksandr.pelykh@gmail.com>
2025-03-21 17:51:19 +0200

    move InceptionVault_S to typescript

--- projects/vaults/test/InceptionVault_S.ts
+++ projects/vaults/test/InceptionVault_S.ts
@@ -1,7 +1,7 @@
 // Tests for InceptionVault_S contract;
 // The S in name does not mean only Symbiotic; this file contains tests for Symbiotic and Mellow adapters
 
-import helpers from '@nomicfoundation/hardhat-network-helpers';
+import * as helpers from "@nomicfoundation/hardhat-network-helpers";
 import hardhat from 'hardhat';
 const { ethers, upgrades, network } = hardhat;
 import { expect } from 'chai';
@@ -16,51 +16,54 @@ import {
   randomAddress,
   e18,
 } from './helpers/utils.js';
+import { stETH } from './src/test-data/assets/inception-vault-s';
 BigInt.prototype.format = function() {
   return this.toLocaleString("de-DE");
 };
 
+// const assets = [
+//   {
+//     assetName: "stETH",
+//     assetAddress: "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",
+//     vaultName: "InstEthVault",
+//     vaultFactory: "InVault_S_E2",
+//     iVaultOperator: "0xd87D15b80445EC4251e33dBe0668C335624e54b7",
+//     ratioErr: 3n,
+//     transactErr: 5n,
+//     blockNumber: 21850700, //21687985,
+//     impersonateStaker: async function(staker, iVault) {
+//       const donor = await impersonateWithEth("0x43594da5d6A03b2137a04DF5685805C676dEf7cB", toWei(1));
+//       const stEth = await ethers.getContractAt("stETH", "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84");
+//       const stEthAmount = toWei(1000);
+//       await stEth.connect(donor).approve(this.assetAddress, stEthAmount);
 
-const assets = [
-  {
-    assetName: "stETH",
-    assetAddress: "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",
-    vaultName: "InstEthVault",
-    vaultFactory: "InVault_S_E2",
-    iVaultOperator: "0xd87D15b80445EC4251e33dBe0668C335624e54b7",
-    ratioErr: 3n,
-    transactErr: 5n,
-    blockNumber: 21850700, //21687985,
-    impersonateStaker: async function(staker, iVault) {
-      const donor = await impersonateWithEth("0x43594da5d6A03b2137a04DF5685805C676dEf7cB", toWei(1));
-      const stEth = await ethers.getContractAt("stETH", "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84");
-      const stEthAmount = toWei(1000);
-      await stEth.connect(donor).approve(this.assetAddress, stEthAmount);
+//       const wstEth = await ethers.getContractAt("IWSteth", this.assetAddress);
+//       const balanceBefore = await wstEth.balanceOf(donor.address);
+//       await wstEth.connect(donor).wrap(stEthAmount);
+//       const balanceAfter = await wstEth.balanceOf(donor.address);
 
-      const wstEth = await ethers.getContractAt("IWSteth", this.assetAddress);
-      const balanceBefore = await wstEth.balanceOf(donor.address);
-      await wstEth.connect(donor).wrap(stEthAmount);
-      const balanceAfter = await wstEth.balanceOf(donor.address);
+//       const wstAmount = balanceAfter - balanceBefore;
+//       await wstEth.connect(donor).transfer(staker.address, wstAmount);
+//       await wstEth.connect(staker).approve(await iVault.getAddress(), wstAmount);
+//       return staker;
+//     },
+//     addRewardsMellowVault: async function(amount, mellowVault) {
+//       const donor = await impersonateWithEth("0x43594da5d6A03b2137a04DF5685805C676dEf7cB", toWei(1));
+//       const stEth = await ethers.getContractAt("stETH", "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84");
+//       await stEth.connect(donor).approve(this.assetAddress, amount);
 
-      const wstAmount = balanceAfter - balanceBefore;
-      await wstEth.connect(donor).transfer(staker.address, wstAmount);
-      await wstEth.connect(staker).approve(await iVault.getAddress(), wstAmount);
-      return staker;
-    },
-    addRewardsMellowVault: async function(amount, mellowVault) {
-      const donor = await impersonateWithEth("0x43594da5d6A03b2137a04DF5685805C676dEf7cB", toWei(1));
-      const stEth = await ethers.getContractAt("stETH", "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84");
-      await stEth.connect(donor).approve(this.assetAddress, amount);
+//       const wstEth = await ethers.getContractAt("IWSteth", this.assetAddress);
+//       const balanceBefore = await wstEth.balanceOf(donor);
+//       await wstEth.connect(donor).wrap(amount);
+//       const balanceAfter = await wstEth.balanceOf(donor);
+//       const wstAmount = balanceAfter - balanceBefore;
+//       await wstEth.connect(donor).transfer(mellowVault, wstAmount);
+//     },
+//   },
+// ];
 
-      const wstEth = await ethers.getContractAt("IWSteth", this.assetAddress);
-      const balanceBefore = await wstEth.balanceOf(donor);
-      await wstEth.connect(donor).wrap(amount);
-      const balanceAfter = await wstEth.balanceOf(donor);
-      const wstAmount = balanceAfter - balanceBefore;
-      await wstEth.connect(donor).transfer(mellowVault, wstAmount);
-    },
-  },
-];
+const assetInfo = stETH;
+
 let MAX_TARGET_PERCENT;
 let emptyBytes = [
   "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
@@ -257,7 +260,7 @@ const initVault = async a => {
   return [iToken, iVault, ratioFeed, asset, iVaultOperator, mellowAdapter, symbioticAdapter, iLibrary, withdrawalQueue];
 };
 
-assets.forEach(function(a) {
+[stETH].forEach(function(a) {
   describe(`Inception Symbiotic Vault ${a.assetName}`, function() {
     this.timeout(150000);
     let iToken, iVault, ratioFeed, asset, mellowAdapter, symbioticAdapter, iLibrary, withdrawalQueue;
