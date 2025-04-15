c8c1f66f70bd58b1137ab862b9690771dffd6961
mellaught <torres-dan@yandex.ru>
2025-04-15 18:48:38 +0300

    mellow multiVault

--- projects/vaults/test/InceptionVault_S.js
+++ projects/vaults/test/InceptionVault_S.js
@@ -22,90 +22,96 @@ BigInt.prototype.format = function () {
 
 const assets = [
   {
-    assetName: "stETH",
-    assetAddress: "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",
-    vaultName: "InstEthVault",
-    vaultFactory: "InVault_S_E2",
+    assetName: "LBTC",
+    assetAddress: "0x7DD12E437C226018A374Bfa2BdAf48340B9942bC",
+    vaultName: "inUSBDVault_S",
+    vaultFactory: "InVault_S_E1",
     iVaultOperator: "0xd87D15b80445EC4251e33dBe0668C335624e54b7",
     ratioErr: 3n,
     transactErr: 5n,
-    blockNumber: 21687985,
+    // blockNumber: 21687985,
     impersonateStaker: async function (staker, iVault) {
-      const donor = await impersonateWithEth("0x43594da5d6A03b2137a04DF5685805C676dEf7cB", toWei(1));
-      const stEth = await ethers.getContractAt("stETH", "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84");
-      const stEthAmount = toWei(1000);
-      await stEth.connect(donor).approve(this.assetAddress, stEthAmount);
+      const assetAddress = "0x8236a87084f8B84306f72007F36F2618A5634494";
+      const donorAddress = "0x3752edF4Ca393a1497F7C50298FbBC4F3EF40368";
+      const amount = 100 * 1e8;
 
-      const wstEth = await ethers.getContractAt("IWSteth", this.assetAddress);
-      const balanceBefore = await wstEth.balanceOf(donor.address);
-      await wstEth.connect(donor).wrap(stEthAmount);
-      const balanceAfter = await wstEth.balanceOf(donor.address);
+      const donor = await impersonateWithEth(donorAddress, toWei(1));
+      const lbtc = await ethers.getContractAt("LBTC", assetAddress);
 
-      const wstAmount = balanceAfter - balanceBefore;
-      await wstEth.connect(donor).transfer(staker.address, wstAmount);
-      await wstEth.connect(staker).approve(await iVault.getAddress(), wstAmount);
+      await lbtc.connect(donor).transfer(staker.address, amount);
+      await lbtc.connect(staker).approve(await iVault.getAddress(), amount);
+
       return staker;
     },
     addRewardsMellowVault: async function (amount, mellowVault) {
-      const donor = await impersonateWithEth("0x43594da5d6A03b2137a04DF5685805C676dEf7cB", toWei(1));
-      const stEth = await ethers.getContractAt("stETH", "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84");
-      await stEth.connect(donor).approve(this.assetAddress, amount);
-
-      const wstEth = await ethers.getContractAt("IWSteth", this.assetAddress);
-      const balanceBefore = await wstEth.balanceOf(donor);
-      await wstEth.connect(donor).wrap(amount);
-      const balanceAfter = await wstEth.balanceOf(donor);
-      const wstAmount = balanceAfter - balanceBefore;
-      await wstEth.connect(donor).transfer(mellowVault, wstAmount);
+      const assetAddress = "0x8236a87084f8B84306f72007F36F2618A5634494";
+      const donorAddress = "0x3752edF4Ca393a1497F7C50298FbBC4F3EF40368";
+      const donor = await impersonateWithEth(donorAddress, toWei(1));
+      const lbtc = await ethers.getContractAt("LBTC", assetAddress);
+      await lbtc.connect(donor).transfer(mellowVault, amount);
     },
   },
+  // {
+  //   assetName: "stETH",
+  //   assetAddress: "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",
+  //   vaultName: "InstEthVault",
+  //   vaultFactory: "InVault_S_E2",
+  //   iVaultOperator: "0xd87D15b80445EC4251e33dBe0668C335624e54b7",
+  //   ratioErr: 3n,
+  //   transactErr: 5n,
+  //   blockNumber: 21687985,
+  //   impersonateStaker: async function (staker, iVault) {
+  //     const donor = await impersonateWithEth("0x43594da5d6A03b2137a04DF5685805C676dEf7cB", toWei(1));
+  //     const stEth = await ethers.getContractAt("stETH", "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84");
+  //     const stEthAmount = toWei(1000);
+  //     await stEth.connect(donor).approve(this.assetAddress, stEthAmount);
+
+  //     const wstEth = await ethers.getContractAt("IWSteth", this.assetAddress);
+  //     const balanceBefore = await wstEth.balanceOf(donor.address);
+  //     await wstEth.connect(donor).wrap(stEthAmount);
+  //     const balanceAfter = await wstEth.balanceOf(donor.address);
+
+  //     const wstAmount = balanceAfter - balanceBefore;
+  //     await wstEth.connect(donor).transfer(staker.address, wstAmount);
+  //     await wstEth.connect(staker).approve(await iVault.getAddress(), wstAmount);
+  //     return staker;
+  //   },
+  //   addRewardsMellowVault: async function (amount, mellowVault) {
+  //     const donor = await impersonateWithEth("0x43594da5d6A03b2137a04DF5685805C676dEf7cB", toWei(1));
+  //     const stEth = await ethers.getContractAt("stETH", "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84");
+  //     await stEth.connect(donor).approve(this.assetAddress, amount);
+
+  //     const wstEth = await ethers.getContractAt("IWSteth", this.assetAddress);
+  //     const balanceBefore = await wstEth.balanceOf(donor);
+  //     await wstEth.connect(donor).wrap(amount);
+  //     const balanceAfter = await wstEth.balanceOf(donor);
+  //     const wstAmount = balanceAfter - balanceBefore;
+  //     await wstEth.connect(donor).transfer(mellowVault, wstAmount);
+  //   },
+  // },
 ];
 let MAX_TARGET_PERCENT;
 
 //https://docs.mellow.finance/mellow-lrt-lst-primitive/contract-deployments
 const mellowVaults = [
   {
-    name: "P2P",
-    vaultAddress: "0x7a4EffD87C2f3C55CA251080b1343b605f327E3a",
+    name: "rsUSBD",
+    vaultAddress: "0x7DD12E437C226018A374Bfa2BdAf48340B9942bC",
     wrapperAddress: "0x41A1FBEa7Ace3C3a6B66a73e96E5ED07CDB2A34d",
     bondStrategyAddress: "0xA0ea6d4fe369104eD4cc18951B95C3a43573C0F6",
     curatorAddress: "0x4a3c7F2470Aa00ebE6aE7cB1fAF95964b9de1eF4",
     configuratorAddress: "0x84b240E99d4C473b5E3dF1256300E2871412dDfe",
   },
-  {
-    name: "Mev Capital",
-    vaultAddress: "0x5fD13359Ba15A84B76f7F87568309040176167cd",
-    wrapperAddress: "0xdC1741f9bD33DD791942CC9435A90B0983DE8665",
-    bondStrategyAddress: "0xc3A149b5Ca3f4A5F17F5d865c14AA9DBb570F10A",
-    curatorAddress: "0xA1E38210B06A05882a7e7Bfe167Cd67F07FA234A",
-    configuratorAddress: "0x2dEc4fDC225C1f71161Ea481E23D66fEaAAE2391",
-  },
-  {
-    name: "Re7",
-    vaultAddress: "0x84631c0d0081FDe56DeB72F6DE77abBbF6A9f93a",
-    wrapperAddress: "0x70cD3464A41B6692413a1Ba563b9D53955D5DE0d",
-    bondStrategyAddress: "0xcE3A8820265AD186E8C1CeAED16ae97176D020bA",
-    curatorAddress: "0xE86399fE6d7007FdEcb08A2ee1434Ee677a04433",
-    configuratorAddress: "0x214d66d110060dA2848038CA0F7573486363cAe4",
-  },
 ];
 
 const symbioticVaults = [
   {
-    name: "Gauntlet Restaked wstETH",
-    vaultAddress: "0xc10A7f0AC6E3944F4860eE97a937C51572e3a1Da",
-    collateral: "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",
-    burner: "0xDB0737bd7eBEA50135e4c8af56900b029b858371",
-    delegator: "0x1f16782a9b75FfFAD87e7936791C672bdDBCb8Ec",
-    slasher: "0x541c86eb2C5e7F3E0C04eF82aeb68EA6A86409ef",
-  },
-  {
-    name: "Ryabina wstETH",
-    vaultAddress: "0x93b96D7cDe40DC340CA55001F46B3B8E41bC89B4",
-    collateral: "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",
-    burner: "0x80918bcD2d1e343ed46E201CD09238149dB5A5bF",
-    delegator: "0x742DD9676086579994E9a3DD536C9CCc0Cc6e78D",
-    slasher: "0xCCA42120Dc4fc945F2fBd227d7D9EA5963bba490",
+    name: "Restaked LsETH Vault",
+    vaultAddress: "0x0FDf3B986d62bE6aE1D5228e5DA90ff6f00c15F6",
+    collateral: "0x7DD12E437C226018A374Bfa2BdAf48340B9942bC",
+    burner: "0x1575Fe670D272E32D656e401EC313662A0AdEB24",
+    delegator: "0x978F96A24bCA1ac13129C85F5BEdBA49b01e9335",
+    slasher: "0xC1c5aed50F817d161b72c34942497C3f77B0Ec36",
   },
 ];
 
@@ -451,7 +457,9 @@ assets.forEach(function (a) {
         const amount = await symbioticRestaker.getDeposited(symbioticVaults[0].vaultAddress);
         const amount2 = await symbioticRestaker.getDeposited(symbioticVaults[1].vaultAddress);
         await iVault.connect(iVaultOperator).undelegateFromSymbiotic(symbioticVaults[0].vaultAddress, amount / 2n);
-        await iVault.connect(iVaultOperator).undelegateFromSymbiotic(symbioticVaults[0].vaultAddress, amount - (amount / 2n));
+        await iVault
+          .connect(iVaultOperator)
+          .undelegateFromSymbiotic(symbioticVaults[0].vaultAddress, amount - amount / 2n);
         await iVault.connect(iVaultOperator).undelegateFromSymbiotic(symbioticVaults[1].vaultAddress, amount2);
 
         symbioticVaultEpoch1 = symbioticVaults[0].vault.currentEpoch() + 1n;
