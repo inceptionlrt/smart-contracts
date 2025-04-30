303f2bd283163da43ced5b10a298a95ce0001274
olexandr13 <oleksandr.pelykh@gmail.com>
2025-04-30 17:54:20 +0300

    add multiple adapters to stETH asset

--- projects/vaults/test/data/assets/new/stETH.ts
+++ projects/vaults/test/data/assets/new/stETH.ts
@@ -27,7 +27,23 @@ export const stETH = {
         bondStrategyAddress: "0xA0ea6d4fe369104eD4cc18951B95C3a43573C0F6",
         curatorAddress: "0x4a3c7F2470Aa00ebE6aE7cB1fAF95964b9de1eF4",
         configuratorAddress: "0x84b240E99d4C473b5E3dF1256300E2871412dDfe",
-      }
+      },
+      {
+        name: "Mev Capital",
+        vaultAddress: "0x5fD13359Ba15A84B76f7F87568309040176167cd",
+        wrapperAddress: "0xdC1741f9bD33DD791942CC9435A90B0983DE8665",
+        bondStrategyAddress: "0xc3A149b5Ca3f4A5F17F5d865c14AA9DBb570F10A",
+        curatorAddress: "0xA1E38210B06A05882a7e7Bfe167Cd67F07FA234A",
+        configuratorAddress: "0x2dEc4fDC225C1f71161Ea481E23D66fEaAAE2391",
+      },
+      {
+        name: "Re7",
+        vaultAddress: "0x84631c0d0081FDe56DeB72F6DE77abBbF6A9f93a",
+        wrapperAddress: "0x70cD3464A41B6692413a1Ba563b9D53955D5DE0d",
+        bondStrategyAddress: "0xcE3A8820265AD186E8C1CeAED16ae97176D020bA",
+        curatorAddress: "0xE86399fE6d7007FdEcb08A2ee1434Ee677a04433",
+        configuratorAddress: "0x214d66d110060dA2848038CA0F7573486363cAe4",
+      },
     ],
     symbiotic: [
       {
@@ -37,7 +53,15 @@ export const stETH = {
         burner: "0xDB0737bd7eBEA50135e4c8af56900b029b858371",
         delegator: "0x1f16782a9b75FfFAD87e7936791C672bdDBCb8Ec",
         slasher: "0x541c86eb2C5e7F3E0C04eF82aeb68EA6A86409ef",
-      }
+      },
+      {
+        name: "Ryabina wstETH",
+        vaultAddress: "0x93b96D7cDe40DC340CA55001F46B3B8E41bC89B4",
+        collateral: "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0",
+        burner: "0x80918bcD2d1e343ed46E201CD09238149dB5A5bF",
+        delegator: "0x742DD9676086579994E9a3DD536C9CCc0Cc6e78D",
+        slasher: "0xCCA42120Dc4fc945F2fBd227d7D9EA5963bba490",
+      },
     ]
   },
 };
