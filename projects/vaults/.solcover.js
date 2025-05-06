967396c1d37e91563a587bec3fe885e6f73ec5d8
olexandr13 <oleksandr.pelykh@gmail.com>
2025-05-06 16:54:40 +0300

    add solcover config

--- projects/vaults/.solcover.js
+++ projects/vaults/.solcover.js
@@ -1,3 +1,12 @@
 module.exports = {
-  skipFiles: ["tests/"],
+  skipFiles: [
+    'tests/',
+    'restakers/',
+    'vaults/EigenLayer/',
+    'vaults/EigenLayer/facets/',
+    'vaults/EigenLayer/facets/ERC4626Facet/',
+    'vaults/Symbiotic/vault_e2/InVault_S_E2.sol',
+    'lib/FullMath.sol'
+  ],
 };
+
