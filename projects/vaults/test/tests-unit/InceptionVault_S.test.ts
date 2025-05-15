ca25e4612896dfb72617ca971ddd27d6e6e95a76
olexandr13 <oleksandr.pelykh@gmail.com>
2025-05-15 16:08:47 +0300

    add tests for migrateDepositBonus

--- projects/vaults/test/tests-unit/InceptionVault_S.test.ts
+++ projects/vaults/test/tests-unit/InceptionVault_S.test.ts
@@ -6,15 +6,21 @@ import { e18, toWei } from "../helpers/utils";
 import { initVault } from "../src/init-vault-new";
 const { ethers, network } = hardhat;
 import { testrunConfig } from '../testrun.config';
+import { adapters, emptyBytes } from "../src/constants";
 
 const assetData = testrunConfig.assetData;
+const symbioticVaults = assetData.adapters.symbiotic;
 
 describe(`Inception Symbiotic Vault ${assetData.asset.name}`, function () {
   let iVault;
   let asset;
   let staker: HardhatEthersSigner, staker2: HardhatEthersSigner;
   let transactErr: bigint;
-  let snapshot: helpers.SnapshotRestorer
+  let snapshot: helpers.SnapshotRestorer;
+  let ratioFeed;
+  let iToken;
+  let iVaultOperator;
+  let symbioticAdapter;
 
   before(async function () {
     if (process.env.ASSETS) {
@@ -32,7 +38,7 @@ describe(`Inception Symbiotic Vault ${assetData.asset.name}`, function () {
       },
     }]);
 
-    ({ iVault, asset } = await initVault(assetData));
+    ({ iToken, iVault, iVaultOperator, asset, ratioFeed, symbioticAdapter } = await initVault(assetData, { adapters: [adapters.Symbiotic] }));
     transactErr = assetData.transactErr;
 
     [, staker, staker2] = await ethers.getSigners();
@@ -225,6 +231,90 @@ describe(`Inception Symbiotic Vault ${assetData.asset.name}`, function () {
 
       expect(iVaultDecimals).to.be.eq(tokenDecimals);
       expect(iVaultDecimals).to.be.eq(18n);
+    });
+  });
+
+  describe('migrateDepositBonus method', () => {
+    // beforeEach(async function () {
+    before(async function () {
+      await snapshot.restore();
+      await iVault.setTargetFlashCapacity(100n);
+    });
+
+    it('should migrate deposit bonus to a new vault and emit event', async () => {
+      // Arrange
+      // set ratio to 1:1
+      await ratioFeed.updateRatioBatch([iToken.address], [toWei(1)]);
+
+      // deposit
+      let depositTx = await iVault.connect(staker).deposit(toWei(50), staker.address);
+      await depositTx.wait();
+
+      // flash withdraw (to generate deposit bonus)
+      let flashWithdrawTx =
+        await iVault.connect(staker)["flashWithdraw(uint256,address,uint256)"](toWei(25), staker.address, 0n);
+      await flashWithdrawTx.wait();
+
+      // Assert: check deposit bonus
+      const depositBonusAmount = await iVault.depositBonusAmount();
+      expect(depositBonusAmount).to.be.gt(0);
+
+      // Act
+      const { iVault: iVaultNew } = await initVault(assetData);
+      const migrateTx = await (await iVault.migrateDepositBonus(await iVaultNew.getAddress())).wait();
+
+      // Assert: bonus migrated
+      const oldDepositBonus = await iVault.depositBonusAmount();
+      expect(oldDepositBonus, 'Old vault deposit bonus should equal 0').to.be.eq(0);
+
+      const newVaultBalance = await asset.balanceOf(iVaultNew.address);
+      expect(newVaultBalance, 'New vault balance should equal to transferred deposit bonus').to.be.eq(depositBonusAmount);
+
+      // Assert: event emitted
+      await expect(migrateTx)
+        .to.emit(iVault, 'DepositBonusTransferred')
+        .withArgs(iVaultNew.address, depositBonusAmount);
+    });
+
+    it('should revert if the new vault address is zero', async () => {
+      await expect(iVault.migrateDepositBonus(ethers.ZeroAddress)).to.be.revertedWithCustomError(
+        iVault,
+        'InvalidAddress'
+      );
+    });
+
+    it('should revert if there is no deposit bonus to migrate', async () => {
+      expect(await iVault.depositBonusAmount(), 'Deposit bonus should be 0').to.be.eq(0);
+
+      await expect(iVault.migrateDepositBonus(staker.address)).to.be.revertedWithCustomError(iVault, 'NullParams');
+    });
+
+    it('should revert if there are delegated funds', async () => {
+      // Arrange
+      // deposit + delegate
+      const depositAmount = toWei(10);
+      await (await iVault.connect(staker).deposit(depositAmount, staker.address)).wait();
+      await (await iVault.connect(iVaultOperator)
+        .delegate(symbioticAdapter.address, symbioticVaults[0].vaultAddress, depositAmount, emptyBytes)).wait();
+
+      // flash withdraw (to generate deposit bonus)
+      let flashWithdrawTx =
+        await iVault.connect(staker)["flashWithdraw(uint256,address,uint256)"](toWei(9), staker.address, 0n);
+      await flashWithdrawTx.wait();
+
+      const depositBonusAmount = await iVault.depositBonusAmount();
+      expect(depositBonusAmount, 'Deposit bonus should exist').to.be.gt(0);
+
+      const { iVault: iVaultNew } = await initVault(assetData);
+
+      // Act/Assert
+      await expect(iVault.migrateDepositBonus(iVaultNew.address)).to.be.revertedWithCustomError(iVault, 'ValueZero');
+    });
+
+    it('should only allow the owner to migrate the deposit bonus', async () => {
+      await expect(iVault.connect(staker).migrateDepositBonus(staker.address)).to.be.revertedWith(
+        'Ownable: caller is not the owner'
+      );
     });
   });
 });
