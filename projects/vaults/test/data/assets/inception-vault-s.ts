876a1da6abd30abb583a81bec0aa3f728334c601
Kamil Mukhametzyanov <grkamil@yandex.ru>
2025-05-19 16:47:19 +0300

    fix tests

--- projects/vaults/test/data/assets/inception-vault-s.ts
+++ projects/vaults/test/data/assets/inception-vault-s.ts
@@ -3,7 +3,7 @@ import hardhat from "hardhat";
 import { impersonateWithEth, toWei } from '../../helpers/utils';
 const { ethers } = hardhat;
 
-const donorAddress = '0x43594da5d6A03b2137a04DF5685805C676dEf7cB';
+const donorAddress = '0x5313b39bf226ced2332C81eB97BB28c6fD50d1a3';
 const stETHAddress = '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84'; // Lido stETH
 
 export const stETH = {
@@ -14,34 +14,18 @@ export const stETH = {
   iVaultOperator: "0xd87D15b80445EC4251e33dBe0668C335624e54b7",
   ratioErr: 3n,
   transactErr: 5n,
-  blockNumber: 21850700, //21687985,
+  blockNumber: 22516224, //21687985,
   impersonateStaker: async function (staker, iVault) {
     const donor = await impersonateWithEth(donorAddress, toWei(1));
-    const stEth = await ethers.getContractAt("stETH", stETHAddress);
-    const stEthAmount = toWei(1000);
-    await stEth.connect(donor).approve(this.assetAddress, stEthAmount);
-
     const wstEth = await ethers.getContractAt("IWSteth", this.assetAddress);
-    const balanceBefore = await wstEth.balanceOf(donor.address);
-    await wstEth.connect(donor).wrap(stEthAmount);
-    const balanceAfter = await wstEth.balanceOf(donor.address);
-
-    const wstAmount = balanceAfter - balanceBefore;
-    await wstEth.connect(donor).transfer(staker.address, wstAmount);
-    await wstEth.connect(staker).approve(await iVault.getAddress(), wstAmount);
+    await wstEth.connect(donor).transfer(staker.address, toWei(1000));
+    await wstEth.connect(staker).approve(await iVault.getAddress(), toWei(1000));
     return staker;
   },
   addRewardsMellowVault: async function (amount, mellowVault) {
     const donor = await impersonateWithEth(donorAddress, toWei(1));
-    const stEth = await ethers.getContractAt("stETH", stETHAddress);
-    await stEth.connect(donor).approve(this.assetAddress, amount);
-
     const wstEth = await ethers.getContractAt("IWSteth", this.assetAddress);
-    const balanceBefore = await wstEth.balanceOf(donor);
-    await wstEth.connect(donor).wrap(amount);
-    const balanceAfter = await wstEth.balanceOf(donor);
-    const wstAmount = balanceAfter - balanceBefore;
-    await wstEth.connect(donor).transfer(mellowVault, wstAmount);
+    await wstEth.connect(donor).transfer(mellowVault, amount);
   },
   applySymbioticSlash: async function (symbioticVault, slashAmount) {
     const slasherAddressStorageIndex = 3;
